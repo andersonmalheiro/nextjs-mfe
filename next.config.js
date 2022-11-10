58c7e8c9e91a53a1a46942d95d364390680034d7
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-11-10 17:26:14 -0300

    updating config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
