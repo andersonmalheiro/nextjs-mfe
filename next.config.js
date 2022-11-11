81c70ed08173a49c856d98c76bf19ff580d006fa
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-11-10 22:52:15 -0300

    adding federation settings

--- next.config.js
+++ next.config.js
@@ -1,17 +1,37 @@
 /** @type {import('next').NextConfig} */
 const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
 // this enables you to use import() and the webpack parser
 // loading remotes on demand, not ideal for SSR
 const remotes = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
+
   return {
-    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    Home: `Home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
   };
 };
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "next_mf",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./App": "./pages/index.tsx",
+        },
+        remotes: {},
+        shared: {},
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
